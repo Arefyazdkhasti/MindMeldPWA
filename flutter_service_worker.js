66cc52c935f92419a2c8f3a8770a4e6042180639
aref <aref.yazdkhasti@gmail.com>
2024-09-25 22:12:24 +0330

    add index

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -44,7 +44,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bffc539e46db08a188b344ea79abf17d
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "1e1aaf5e723ff6beae588c0e48b497fb",
+"flutter_bootstrap.js": "fdaf1ab986bf4f379568cb6265e7c699",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
